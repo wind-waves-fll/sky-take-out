5bac6d245a63c58ed94e2af4a9787f46b5a4eafb
wind-waves-fll <lulinfan0514@gmail.com>
2024-11-07 21:32:47 +0800

    新增员工业务代码开发

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.sky.handler;
 
+import com.sky.constant.MessageConstant;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
@@ -23,5 +26,23 @@ public class GlobalExceptionHandler {
         log.error("异常信息：{}", ex.getMessage());
         return Result.error(ex.getMessage());
     }
-
+    /**
+     * 处理sql异常
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler
+    public Result exceptionHandler(SQLIntegrityConstraintViolationException ex){
+        //Duplicate entry '肇国珍' for key 'employee.idx_username'
+        String msg = ex.getMessage();
+        if(msg.contains("Duplicate entry")){
+            String[] s = msg.split(" ");
+            String name = s[2];
+            String message = name + MessageConstant.ALREADY_EXISTS;
+            return Result.error(message);
+        }
+        else {
+            return Result.error(MessageConstant.UNKNOWN_ERROR);
+        }
+    }
 }
